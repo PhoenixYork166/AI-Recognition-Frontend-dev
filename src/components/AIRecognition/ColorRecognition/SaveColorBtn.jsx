d728c47f880723b551df6910811225d721e7eadd
phoenixyork166 <berlinlee2017@gmail.com>
2023-06-19 17:17:34 +0800

    fixing

--- src/components/AIRecognition/ColorRecognition/SaveColorBtn.jsx
+++ src/components/AIRecognition/ColorRecognition/SaveColorBtn.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const SaveColorBtn = ( { name, onSaveColorButton }) => {
 
     return (
@@ -14,21 +13,4 @@ const SaveColorBtn = ( { name, onSaveColorButton }) => {
 }
 
 
-=======
-const SaveColorBtn = ( { name, onSaveColorButton }) => {
-
-    return (
-        <div className='container'>
-        <input
-            type='button'
-           className='saveBtnText'
-           value={`${name} :D\nSave Raw Hex to your Profile`}
-           onClick={onSaveColorButton}
-        />
-        </div>
-    )
-}
-
-
->>>>>>> d7f1c00d012083fb6955d68e956c0eead0a877b1
 export default SaveColorBtn;
